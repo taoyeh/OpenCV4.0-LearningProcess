76fc17de0105fbe543c51ca7d62714b2d88a9690
taoyeh <xxx@xxx.com>
2021-04-08 14:38:45 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src =imread("D:/test.jpg");
+	imshow("input", src);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
